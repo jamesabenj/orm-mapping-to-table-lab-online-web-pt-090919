136fa1256e0d91c607018c058de5f69cfb04c0e8
James Benjamin <james.abenj@gmail.com>
2019-12-03 21:48:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade 
   attr_reader :id
 
-  def intiialize(name, grade)
+  def intiialize(name*, grade*, id*)
     @id = id
     @name = name
     @grade = grade
