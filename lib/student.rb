6b95a5c2f115c5933b6682e58688b443a1997ac7
James Benjamin <james.abenj@gmail.com>
2019-12-03 21:42:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,17 +4,17 @@ class Student
 
   def intiialize(name, grade, id = nil)
     @id = id
-    @name = name 
-    @grade = grade 
+    @name = name
+    @grade = grade
   end
 
-  def name 
+  def name
     @name
-  end 
+  end
 
-  def grade 
-    @grade 
-  end 
+  def grade
+    @grade
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
